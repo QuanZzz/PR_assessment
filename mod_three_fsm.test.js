0c6fe90f88df05575e46423fbf9009747eb25413
QuanZzz <zhouquaneric@gmail.com>
2023-11-28 01:14:49 -0500

    unit test tweaks

--- mod_three_fsm.test.js
+++ mod_three_fsm.test.js
@@ -4,7 +4,7 @@ const {
   updateTransition
 } = require('./mod_three_fsm');
 
-describe('Mod Three State Transition Tests', () => {
+describe('Mod Three State Tests', () => {
   test('Valid input string "110"', () => {
     const inputString = '110';
     const expectedOutput = OUTPUT[STATES.S0];
@@ -14,36 +14,30 @@ describe('Mod Three State Transition Tests', () => {
   test('Valid input string "1010"', () => {
     const inputString = '1010';
     const expectedOutput = OUTPUT[STATES.S1];
-    expect(updateTransition(inputString)).toEqual(expectedOutput);
+    expect(updateTransition(inputString)).toBe(expectedOutput);
   });
 
-  test('Invalid input string "ABC"', () => {
-    const inputString = 'ABC';
+  test('Invalid input string "AGDBRHGF"', () => {
+    const inputString = 'AGDBRHGF';
     const expectedOutput = undefined;
-    expect(updateTransition(inputString)).toEqual(expectedOutput);
+    expect(updateTransition(inputString)).toBe(expectedOutput);
   });
 
   test('Empty input string', () => {
     const inputString = '';
     const expectedOutput = OUTPUT[STATES.S0];
-    expect(updateTransition(inputString)).toEqual(expectedOutput);
+    expect(updateTransition(inputString)).toBe(expectedOutput);
   });
 
   test('Input string leading to initial state', () => {
     const inputString = '000';
     const expectedOutput = OUTPUT[STATES.S0];
-    expect(updateTransition(inputString)).toEqual(expectedOutput);
+    expect(updateTransition(inputString)).toBe(expectedOutput);
   });
 
   test('Input string leading to final state', () => {
     const inputString = '111111';
     const expectedOutput = OUTPUT[STATES.S0];
-    expect(updateTransition(inputString)).toEqual(expectedOutput);
-  });
-
-  test('Input string leading to intermediate state', () => {
-    const inputString = '101010';
-    const expectedOutput = OUTPUT[STATES.S0];
-    expect(updateTransition(inputString)).toEqual(expectedOutput);
+    expect(updateTransition(inputString)).toBe(expectedOutput);
   });
 });
